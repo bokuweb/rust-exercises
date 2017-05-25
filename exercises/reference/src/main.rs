22fd42005302ab76ba690522812d6f73fb521c4c
bokuweb <bokuweb12@gmail.com>
2017-05-25 20:37:16 +0900

    Use slice

--- exercises/reference/src/main.rs
+++ exercises/reference/src/main.rs
@@ -9,12 +9,24 @@
 fn main() {
     let name = format!("dear rustaceans");
     // &mut を使うと参照先の変更も可能
-    let r = &name;
+    let r = &name[..];
+    let s = &name[5..]; // nameの5から末尾
+                        // 範囲は5文字目じゃななくて5byte目
+                        // slice
     greet(r);
-    greet(r);
+    greet(s);
 }
 
-fn greet(name: &String) {
+fn greet(name: &str) {
     // 内部で自動的に参照から実態に変えられる => *name　としなくてよい 
     println!("Hello {}", name);
 }
+
+// &Stringからマジカルに&strになるので引数で受け取るときは&Stringより&strを使う方が便利です
+/*
+format!("こんにちは")
+と
+"こんにちは".to_string()
+は前者はformat!としての機能を活かしてないから同じものと考えてしまっていいのかなあ？
+ => 同じ
+ */
